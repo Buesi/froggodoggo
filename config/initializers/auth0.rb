713ad8925af77a64f1d54eabcf41168f78014b27
Buesi <dorena@humbot.io>
2020-01-16 17:31:40 +0000

    changed auth0 to env

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    'S8wIVZtr5dH3B4ljb91226YSE6eSnN0N',
-    'Q_vqvcTDNUyJ8N6nRNxMC1ZL7EXK1eYTqDmd_A39hSvBDJGDFvOFOQ25_dyR02dO',
-    'froggodoggo-staging.eu.auth0.com',
+    ENV['AUTH0_RUBY_CLIENT_ID'],
+    ENV['AUTH0_RUBY_CLIENT_SECRET'],
+    ENV['AUTH0_RUBY_DOMAIN'],
     callback_path: '/auth/auth0/callback',
     authorize_params: {
       scope: 'openid email profile'
